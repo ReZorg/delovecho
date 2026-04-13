1e209d81c4da09b9c4309baefd4f35ace21f0064
drzo <15202748+drzo@users.noreply.github.com>
2026-04-13 23:27:14 +0200

    uynn

--- packages/ui-components/Live2DAvatar/index.ts
+++ packages/ui-components/Live2DAvatar/index.ts
@@ -5,6 +5,13 @@
 export { Live2DAvatar } from './Live2DAvatar.js';
 export { EndocrineExpressionBridge } from './EndocrineExpressionBridge.js';
 export { MiaraManifest } from './MiaraManifest.js';
+export { AionManifest } from './AionManifest.js';
+export { LorenzChaoticDriver } from './LorenzChaoticDriver.js';
+export {
+  NeuroChaoCognitiveDriver,
+  NEURO_CHAO_DEFAULTS,
+  AION_QUANTUM_DEFAULTS,
+} from './NeuroChaoCognitiveDriver.js';
 export { useScriptLoader } from './useScriptLoader.js';
 
 export type {
@@ -17,3 +24,18 @@ export type {
   ExpressionMapping,
   ScriptLoadState,
 } from './types.js';
+
+export type {
+  LorenzConfig,
+} from './LorenzChaoticDriver.js';
+
+export type {
+  NeuroChaoPersonality,
+  AionQuantumTraits,
+  CognitivePipelinePhase,
+  EchobeatsPhase,
+  FourECognitionMetrics,
+  AestheticParameters,
+  IntrospectiveState,
+  CognitiveState,
+} from './NeuroChaoCognitiveDriver.js';
